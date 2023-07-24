2d76f0dd5697b5722e007b4f9e5209e65adbf1b8
Ayush Bhowate <ayushpay1919@gmail.com>
2023-07-24 19:03:37 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+    cout<<"Enter numbers";
+    cin>>a>>b;
+    sum=a+b;
+    cout<<"Sum is "<<sum;
+
+    return 0;
+}
+ 
